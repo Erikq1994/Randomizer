ad2317bd4b1a10532c3850d9b2cd9fb71050e71b
Erik Quiroz <erikq94@gmail.com>
2018-04-02 11:48:49 -0700

    Not finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #Randomizer
+
+@author - Erik Quiroz
+
+@comment - I solved the solution by facade pattern because any class should be able to create a
+          instance. 
--- Randomize.cpp
+++ Randomize.cpp
@@ -1,2 +1,24 @@
 #include <ctime>
 #include <cstlib>
+
+
+
+unsigned int Randomizer::randomBetween(unsigned int minimum, unsigned int maximum)
+{
+  unsigned int number = 0; //number to be returned in the end.
+  srand(time(0)); //uses the time as a seed to assure that it is a random number every time.
+  unsigned int offset = 0;
+
+  if(minimum > maximum)
+  {
+    number = (rand() % minimum);
+  }
+
+  else
+  {
+    offset = maximum - minimum;
+    number = (rand() % offset) + minimum;
+  }
+
+  return number;
+}
